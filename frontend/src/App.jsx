c30c6bbf45bbd3eee88f3c5e5c1906eda86e24d7
PETEROH018 <mathengep63@gmail.com>
2026-08-20 21:54:46 +0300

    Assigned a route to the ApartmentDetails page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Login from './pages/Login/Auth'
 import Home from '../src/pages/Home/Home'
 import AdminDashboard from '../src/pages/AdminDashboard/Admindashboard'
+import ApartmentDetails from '../src/pages/ApartmentDetails'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Route path='/admin-dash' element={<AdminDashboard/>}/>
       <Route path='/login' element={<Login/>}/>
       <Route path='/' element={<Login/>}/>
+      <Route path='/apartment-details' element={<ApartmentDetails/>}/>
     </Routes>    
     </BrowserRouter>
     </>
