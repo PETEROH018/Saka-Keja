8453c0f788ef2631f345287bf5604f4dd3343eeb
PETEROH018 <mathengep63@gmail.com>
2026-08-21 00:02:05 +0300

    Removed the main element and moved it to WhySakaKeja component so that the home component can accomodate other components like the navbar and the preview section

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,12 +2,8 @@ import WhySakaKeja from "../../components/WhySakaKeja";
 
 export default function Home() {
   return (
-    <main className="bg-surface text-on-surface font-sans min-h-screen">
-      <h1 className="text-3xl font-bold">
-        Saka Keja
-      </h1>
-
+    <>
       <WhySakaKeja />
-      </main>
+    </>
   );
 }
