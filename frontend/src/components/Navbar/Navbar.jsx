d2b72b8385646eee39127d1ffd9af2f95a6660ad
Enoch-Wanjala <enochwanjala.97@gmail.com>
2026-08-20 16:57:42 +0300

    task: add navbar navigation links

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 // Reusable navigation bar 
+import { NavLink } from "react-router-dom"
+
 export default function Navbar() {
   return (
     <nav>
       <div>
         <span>Saka Keja</span>
+      </div>
+
+      <div>
+        <NavLink to="/home">Discover</NavLink>
+        <NavLink to="/favorites">Favorites</NavLink>
+        <NavLink to="/messages">Messages</NavLink>
       </div>
     </nav>
   )
