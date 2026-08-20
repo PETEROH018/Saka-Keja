67bbca68ccc1a1a8724b6a83169c74a2566894fe
Darrel <darrel124568@gmail.com>
2026-08-20 18:39:25 +0300

    refactor: update Home component to use state for endpoint management

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,17 @@
 import UnitsGrid from "../../components/UnitssGrid/UnitsGrid"
 import useFetch from "../../hooks/useFetch";
-
+import { useState } from "react";
 
 export default function Home() {
-    const { data: units, loading, error } = useFetch("http://localhost:3000/featured");
+    const [endpoint, setEndpoint] = useState("http://localhost:3000/featured");
+
+    // After search, the endpoint will be changed triggering another fetch
+
+    const { data: units, loading, error } = useFetch(endpoint);
+
     return (
         <>
         <UnitsGrid units={units} loading={loading} error={error}/>
         </>
-    )
+    );
 }
