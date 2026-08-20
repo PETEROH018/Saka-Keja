3216a3cc625601ecb770aaa5e2aa1a8fada60e76
jst.talel <mathewstalel@outlook.com>
2026-08-20 18:52:06 +0300

    Added searchresults route in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Login from './pages/Login/Auth'
 import Home from '../src/pages/Home/Home'
 import AdminDashboard from '../src/pages/AdminDashboard/Admindashboard'
+import SearchResults from './SearchResults'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -9,13 +10,13 @@ function App() {
   return (
     <>
     <BrowserRouter>
-    <Routes>
-      <Route path='/home' element={<Home/>}/>
-      <Route path='/admin-dash' element={<AdminDashboard/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/' element={<Login/>}/>
-      
-    </Routes>    
+      <Routes>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/admin-dash' element={<AdminDashboard/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/search' element={<SearchResults/>}/>
+        <Route path='/' element={<SearchResults/>}/>
+      </Routes>    
     </BrowserRouter>
     </>
   )
