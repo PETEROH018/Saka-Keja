e2fc0e9232a37b13eaf29da02330ae73d88bd475
PETEROH018 <mathengep63@gmail.com>
2026-08-21 16:53:25 +0300

    Added a state variable that stores apartment details for the apartment being viewed currently

--- frontend/src/pages/ApartmentDetails.jsx
+++ frontend/src/pages/ApartmentDetails.jsx
@@ -1,10 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function ApartmentDetails(){
+    const [apartment,setApartment] = useState()
     useEffect(()=>{
         fetch("http://localhost:3000/apartments/1")
         .then(response=> response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            setApartment(data)
+            console.log(data)
+        })
         .catch(error => console.error(error))
     },[])
+
+    return(
+        <div className="min-h-screen bg-gray-50 font-sans antialiased text-gray-900 pb-12">
+
+        </div>
+    )
 }
