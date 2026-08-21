2692eaa9bda45d2ff08e913b9c25c7cbb8f79f2b
PETEROH018 <mathengep63@gmail.com>
2026-08-21 19:29:31 +0300

    Added a button for setting an appartment to favorite

--- frontend/src/pages/ApartmentDetails.jsx
+++ frontend/src/pages/ApartmentDetails.jsx
@@ -49,6 +49,7 @@ export default function ApartmentDetails(){
     const [showAllPhotos, setShowAllPhotos] = useState(false);
     const [isLoading, setIsLoading] = useState(true)
     const [apartment,setApartment] = useState({})
+    const [isFavorited, setIsFavorited] = useState(false);
   
     useEffect(()=>{
         fetch("http://localhost:3000/apartments/1")
@@ -218,6 +219,33 @@ export default function ApartmentDetails(){
                Contact Owner
               </button>
             </div>
+
+             <button 
+              onClick={() => setIsFavorited(!isFavorited)}
+              className={`w-full border font-semibold py-3 px-4 rounded-xl transition-all flex items-center justify-center gap-2 text-sm shadow-xs ${
+                isFavorited 
+                  ? 'bg-red-50 border-red-200 text-red-600' 
+                  : 'bg-white border-gray-300 text-gray-700 hover:bg-gray-50 active:scale-[0.99]'
+              }`}
+            >
+            <svg 
+                className={`w-4 h-4 transition-colors ${
+                    isFavorited 
+                    ? 'fill-red-500 text-red-500' 
+                    : 'fill-none text-gray-400'
+                }`} 
+                viewBox="0 0 24 24" 
+                stroke="currentColor" 
+                strokeWidth="2"
+                >
+                <path 
+                    strokeLinecap="round" 
+                    strokeLinejoin="round" 
+                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" 
+                />
+            </svg>
+              {isFavorited ? 'Saved to Favorites' : 'Add to Favorites'}
+            </button>
             </aside>
         </div>
         </div>
