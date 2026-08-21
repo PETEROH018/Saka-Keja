6b2b40ced8ea69450a0011be439edaaa3cf566a4
PETEROH018 <mathengep63@gmail.com>
2026-08-21 16:37:32 +0300

    Added a fetch functionality for fetching data from the mock backend

--- frontend/src/pages/ApartmentDetails.jsx
+++ frontend/src/pages/ApartmentDetails.jsx
@@ -1,3 +1,10 @@
+import { useEffect } from "react";
+
 export default function ApartmentDetails(){
-    
+    useEffect(()=>{
+        fetch("http://localhost:3000/apartments/1")
+        .then(response=> response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error))
+    },[])
 }
