fa02ce194ad13c11d93ea873cb0de80b8ffe4bfe
Natalie Kimani <anitakimani07@gmail.com>
2026-08-22 21:39:03 +0300

    Compare page added to pages

--- frontend/src/pages/ComparePage/ComparePage.jsx
+++ frontend/src/pages/ComparePage/ComparePage.jsx
@@ -1,4 +1,4 @@
-import CompareProperties from "../components/CompareProperties/CompareProperties";
+import CompareProperties from "../../components/CompareProperties/CompareProperties";
 
 export default function ComparePage() {
   return <CompareProperties propertyIds={["1", "3", "5"]} />;
