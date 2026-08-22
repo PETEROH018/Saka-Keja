e8999de35692b98117665df1e986bb12c0617093
Natalie Kimani <anitakimani07@gmail.com>
2026-08-22 21:15:05 +0300

    wire CompareProperties into compare route

---
+++ frontend/src/components/CompareProperties/ComparePage.jsx
@@ -0,0 +1,5 @@
+import CompareProperties from "../components/CompareProperties/CompareProperties";
+
+export default function ComparePage() {
+  return <CompareProperties propertyIds={["1", "3", "5"]} />;
+}
