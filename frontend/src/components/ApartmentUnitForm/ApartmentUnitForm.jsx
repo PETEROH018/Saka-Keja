c300cf92bc8aca75744b5f65c844486b9fb49dd2
PETEROH018 <mathengep63@gmail.com>
2026-08-24 00:20:53 +0300

    Added a header section to the AddApartmentUnit  component

---
+++ frontend/src/components/ApartmentUnitForm/ApartmentUnitForm.jsx
@@ -0,0 +1,16 @@
+export default function AddApartmentUnit(){
+  return(
+    <div className="border-b border-[#e4dce8] bg-[#fcf8fd] px-5 pb-6 pt-7 lg:px-7">
+
+        <h1 className="text-[22px] font-bold tracking-tight">
+          Add Units
+        </h1>
+
+        <p className="mt-1 text-[10px] text-[#77717c]">
+          Define the individual units available in this
+          building.
+        </p>
+      </div>
+
+  )
+}
