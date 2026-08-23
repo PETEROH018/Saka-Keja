59700d18a372ebb6f5bea8cc203adb13979e1306
PETEROH018 <mathengep63@gmail.com>
2026-08-24 00:27:21 +0300

    Added functions that handle navigating back and forth between the apartment details form, unit details form and the review section

--- frontend/src/pages/AddApartment/AddApartment.jsx
+++ frontend/src/pages/AddApartment/AddApartment.jsx
@@ -4,7 +4,33 @@ import AdminSideBar from "../../components/AdminSideBar/AdminSideBar";
 
 
 export default function AddApartment(){
+  const [currentStep, setCurrentStep] = useState(1);
 
+  const handleLocationContinue = (locationData) => {
+    // setPropertyData((prev) => ({
+    //   ...prev,
+    //   location: locationData,
+    // }));
+
+    setCurrentStep(2);
+  };
+
+
+  const handleUnitsContinue = (unitsData) => {
+    // setPropertyData((prev) => ({
+    //   ...prev,
+    //   units: unitsData,
+    // }));
+
+    setCurrentStep(3);
+  };
+
+
+  const handleBack = () => {
+    setCurrentStep((prev) =>
+      Math.max(1, prev - 1)
+    );
+  };
     return(
         <div className="min-h-screen bg-[#fcf8fd] text-[#28232d]">
         <div className="flex min-h-screen">
