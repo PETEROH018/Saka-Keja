c00ef9825c426ae28642776dffe1893c1d93aebc
Darrel <darrel124568@gmail.com>
2026-08-23 21:31:10 +0300

    Made the page layout for Ownerprofile

--- frontend/src/pages/OwnerProfile/OwnerProfile.jsx
+++ frontend/src/pages/OwnerProfile/OwnerProfile.jsx
@@ -3,13 +3,33 @@ import Footer from "../../components/Footer/Footer"
 
 function OwnerProfile() {
   return (
-    <div>
-      <AdminSideBar/>
-      <div className="absolute h-full right-0 top-0 bg-red-200 w-[70%]">
-        <div className="w-10 rounded-full bg-gray-900">JD</div>
-      </div>
-      <Footer/>
+    <>
+    <AdminSideBar/>
+    <div className=" absolute right-2 top-0 bg-red-200 h-full w-[65%] p-2">      
+    <div className="bg-red-200 m-4 rounded-lg p-2">
+        <div className="flex h-15 w-15 shrink-0 items-center justify-center rounded-full bg-primary text-sm font-bold text-white shadow-xs">JD </div>
+        <div>
+            <h1>John Doe</h1>
+            <p>Verified property owner</p>
+            <p>Experienced property manager dedicated to providing safe, comfortable and affordable student housing</p>
+            <hr />
+            <div className="flex gap-2">
+                <a href="#">email</a>
+                <p>phone number</p>
+                <p>Joined on</p>
+            </div>
+        </div>
+     </div>
+     <div className="flex gap-2">
+        <div className="p-4 text-white bg-white/20 border border-gray-300 rounded-lg flex flex-col justify-center text-center"><span className="font-bold text-lg">4</span> Listings</div>
+         <div className="p-4 text-white bg-white/20 border border-gray-300 rounded-lg flex flex-col justify-center text-center"><span className="font-bold text-lg">12</span>Active Tenants</div>
+         <div className="p-4 text-white bg-white/20 border border-gray-300 rounded-lg flex flex-col justify-center text-center"><span className="font-bold text-lg">4.8</span> Average Rating</div>
+    </div> 
+    <div>Manage Listings</div>    
     </div>
+    <Footer/>
+    </>
+    
   )
 }
 
