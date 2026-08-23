70ea2683a00ba5cadd39d703869dcebf67f72e43
PETEROH018 <mathengep63@gmail.com>
2026-08-23 19:32:01 +0300

    Added a route for the Add Apartment page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from '../src/pages/Home/Home'
 import AdminDashboard from '../src/pages/AdminDashboard/Admindashboard'
 import ApartmentDetails from '../src/pages/ApartmentDetails'
 import SearchResults from './pages/SearchResults/SearchResults'
+import AddAppartment from './pages/AddAppartment/AddAppartment'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ComparePage from "./pages/Compare/ComparePage";
 
@@ -18,7 +19,8 @@ function App() {
       <Route path='/apartment-details' element={<ApartmentDetails/>}/>
       <Route path='/search' element={<SearchResults/>}/>
       <Route path='/' element={<Login/>}/>
-      <Route path="/compare" element={<ComparePage />} />
+      <Route path='/compare' element={<ComparePage />} />
+      <Route path='/add-apartment' element={<AddAppartment />} />
       </Routes>
     </BrowserRouter>
     </>
