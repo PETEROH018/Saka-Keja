bb04118630b8ea2e5b19e678c0127c14948768ea
PETEROH018 <mathengep63@gmail.com>
2026-08-23 19:28:42 +0300

    Created a component for adding a new apartment and inserted the side bar component

---
+++ frontend/src/pages/AddAppartment/AddAppartment.jsx
@@ -0,0 +1,8 @@
+import AdminSideBar from "../../components/AdminSideBar/AdminSideBar";
+export default function AddAppartment(){
+    return(
+        <>
+        <AdminSideBar />
+        </>
+    )
+}
