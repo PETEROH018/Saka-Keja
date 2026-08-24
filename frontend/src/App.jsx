f34e9d2bc32018f6fd4a43859f260e1d02a2adca
PETEROH018 <mathengep63@gmail.com>
2026-08-24 23:21:13 +0300

    Added a route for the MyProperties page on the manager's view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import AdminDashboard from '../src/pages/AdminDashboard/Admindashboard'
 import ApartmentDetails from '../src/pages/ApartmentDetails'
 import SearchResults from './pages/SearchResults/SearchResults'
 import AddApartment from './pages/AddApartment/AddApartment'
+import MyProperties from './pages/MyProperties/MyProperties.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ComparePage from "./pages/Compare/ComparePage";
 import StudentProfile from "./components/StudentProfile/StudentProfile.jsx";
@@ -20,14 +21,14 @@ function App() {
       <Route path='/admin-dash' element={<AdminDashboard/>}/>
       <Route path='/apartment-details' element={<ApartmentDetails/>}/>
       <Route path='/search' element={<SearchResults/>}/>
-      <Route path='/' element={<Login/>}/>
+      <Route path='/my-properties' element={<MyProperties/>}/>
       <Route path='/compare' element={<ComparePage />} />
       <Route path='/add-apartment' element={<AddApartment />} />
       <Route path="/compare" element={<ComparePage />} />
       <Route path="/student-profile" element={<StudentProfile />} />
       <Route path="/owner-profile" element={<OwnerProfile />} />
       <Route path="/about" element={<About />} />
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Login/>} />
     </Routes>
     </BrowserRouter>
 
