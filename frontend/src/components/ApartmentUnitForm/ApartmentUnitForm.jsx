bc5038e4becd06e05d398fbb6cdd25da42a97f98
PETEROH018 <mathengep63@gmail.com>
2026-08-24 22:16:03 +0300

    Removed console statement

--- frontend/src/components/ApartmentUnitForm/ApartmentUnitForm.jsx
+++ frontend/src/components/ApartmentUnitForm/ApartmentUnitForm.jsx
@@ -175,7 +175,6 @@ export default function ApartmentUnitForm({units,setUnits,onBack,onContinue}){
 
     resetForm();
   };
-console.log(units)
 
   return(
   <div className="border-b border-[#e4dce8] bg-[#fcf8fd] px-5 pb-6 pt-7 lg:px-7">
