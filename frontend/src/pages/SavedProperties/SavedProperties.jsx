50e008aeb671cdf71531b70dc682fdfcfb8ef9f5
jst.talel <mathewstalel@outlook.com>
2026-08-24 19:33:10 +0300

    Included imports in SavedProperties

---
+++ frontend/src/pages/SavedProperties/SavedProperties.jsx
@@ -0,0 +1,4 @@
+import React, { useState, useEffect } from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
+import './SavedProperties.css';
