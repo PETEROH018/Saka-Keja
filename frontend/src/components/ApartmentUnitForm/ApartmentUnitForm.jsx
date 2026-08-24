634915c3d1b91410dff30689344a1906e4b4bfb8
PETEROH018 <mathengep63@gmail.com>
2026-08-24 10:14:31 +0300

    Added a header section for the Apartment Unit Form

--- frontend/src/components/ApartmentUnitForm/ApartmentUnitForm.jsx
+++ frontend/src/components/ApartmentUnitForm/ApartmentUnitForm.jsx
@@ -1,7 +1,14 @@
 export default function ApartmentUnitForm(){
   return(
     <div className="border-b border-[#e4dce8] bg-[#fcf8fd] px-5 pb-6 pt-7 lg:px-7">
+        <h1 className="text-[22px] font-bold tracking-tight">
+          Add Units
+        </h1>
 
+        <p className="mt-1 text-[10px] text-[#77717c]">
+          Add the individual units available in this
+          apartment.
+        </p>
     </div>
 
   )
