5789bd06ce4ed3a3f55ae74ce700bf9a8a51b838
Darrel <darrel124568@gmail.com>
2026-08-25 18:48:56 +0300

    Add validation to fields in StudentSchema for id, fullname, email, phone_number, and username

--- backend/server/schema.py
+++ backend/server/schema.py
@@ -4,10 +4,10 @@ from marshmallow import ValidationError, fields, validate, validates_schema, Sch
 #STUDENT SCHEMA
 #===========
 class StudentSchema(Schema):
-    id = fields.Int(dump_only=True)
-    fullname = fields.Str(required=True)
-    email = fields.Str(required=True)
-    phone_number = fields.Str(required=True)
+    id = fields.Int(dump_only=True, validate=validate.Length(min=1))
+    fullname = fields.Str(required=True, validate=validate.Length(min=1))
+    email = fields.Str(required=True, validate=validate.Length(min=1))
+    phone_number = fields.Str(required=True, validate=validate.Length(min=1))
     dob = fields.DateTime(required=False)
     institution= fields.Str(required=False)
     course = fields.Str(required=False)
@@ -15,7 +15,7 @@ class StudentSchema(Schema):
     student_number = fields.Int(required=False)
     graduation_year = fields.Int(required=False)
     location = fields.Str(required=False)
-    username = fields.Str(required=True)
+    username = fields.Str(required=True, validate=validate.Length(min=1))
     password = fields.Str(
         required=True,
         load_only=True,
