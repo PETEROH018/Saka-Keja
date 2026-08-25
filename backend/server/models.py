eaf264e289fd54fd3b661e41fb093db3cf76a99b
Darrel <darrel124568@gmail.com>
2026-08-25 15:45:51 +0300

    Added imports for Column, relationship, and field types.

--- backend/server/models.py
+++ backend/server/models.py
@@ -1,5 +1,2 @@
-from sqlalchemy import MetaData, create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-meta = MetaData()
-DATABASE_URI = "postgresql://details"
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
