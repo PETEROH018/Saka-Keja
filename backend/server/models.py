d1f24163e670ad4b54ccde29f6a65477b842b720
Darrel <darrel124568@gmail.com>
2026-08-25 15:50:27 +0300

    Refactor models.py to define Base and MetaData for SQLAlchemy integration

--- backend/server/models.py
+++ backend/server/models.py
@@ -1,2 +1,13 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, MetaData
+from sqlalchemy.orm import relationship, declarative_base
+
+meta = MetaData()
+
+Base = declarative_base(metadata=meta)
+
+class Student(Base):
+    __tablename__ = "students"
+
+    id = Column(Integer, primary_key=True)
+    full_name = Column(String(100), nullable=False)
+
