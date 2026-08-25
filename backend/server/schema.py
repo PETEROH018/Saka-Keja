d1c3b776e08e55bea8a29921d2e6c381b6cb001a
PETEROH018 <mathengep63@gmail.com>
2026-08-25 22:48:56 +0300

    Created a schema for the Apartment model using  SQLAlchemyAutoSchema to handle the serialization and deserialization of individual fields

--- backend/server/schema.py
+++ backend/server/schema.py
@@ -2,3 +2,8 @@ from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
 from models import *
 
+class ApartmentSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Apartment
+        load_instance = True  
+        include_fk = True 
