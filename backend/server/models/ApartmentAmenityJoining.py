e5994a1d5831458f2aa3d2bca7aca4f0cc90f9e8
PETEROH018 <mathengep63@gmail.com>
2026-08-26 11:28:20 +0300

    Created an ApartmentAmenityJoining model and created a relationship with the ApartmentAmenity model and the Apartment model

--- backend/server/models/ApartmentAmenityJoining.py
+++ backend/server/models/ApartmentAmenityJoining.py
@@ -1,1 +1,10 @@
 from configs import *
+
+class ApartmentAmenityJoining(db.Model):
+    __tablename__ = "apartment_amenities_joining"
+
+    id = db.Column(db.Integer, primary_key=True)
+    apartmentId = db.Column(db.Integer, db.ForeignKey("apartments.id"), nullable=False)
+    amenityId = db.Column(db.Integer, db.ForeignKey("apartment_amenities.id"), nullable=False)
+
+    amenity = db.relationship("ApartmentAmenity", back_populates="apartment_links")
