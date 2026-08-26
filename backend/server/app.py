a3bad1be3332b6c19a450d3b0917081338abcc35
Darrel <darrel124568@gmail.com>
2026-08-26 16:20:44 +0300

    Refactor app.py by removing unused unit amenities endpoints and cleaning imports

--- backend/server/app.py
+++ backend/server/app.py
@@ -1,97 +1,17 @@
-from models import db, UnitAmenity, UnitAmenityJoining, Unit, Apartment, Unit, StudentUnit
 from configs import *
-from schema import (
-    unit_amenity_schema,
-    unit_amenities_schema,
-    unit_amenity_joining_schema,
-    unit_amenities_joining_schema,
-    ApartmentSchema,
-    UnitSchema
-)
-from sqlalchemy import func, select
+from schema import *
+from models.Unit import Unit
+from models.Apartment import Apartment
+from models.ApartmentAmenity import ApartmentAmenity
+from models.ApartmentAmenityJoining import ApartmentAmenityJoining
+from models.ApartmentOwner import ApartmentOwner
+from models.Student import Student
+from models.NearbyFacility import NearbyFacility
+from models.UnitAmenity import UnitAmenity
+from models.UnitAmenityJoining import UnitAmenityJoining
+from models.StudentUnit import StudentUnit
+from models.Payment import Payment
 
-# ---------- unit_amenities ----------
-
-@app.route("/unit_amenities", methods=["GET"])
-def get_unit_amenities():
-    amenities = UnitAmenity.query.all()
-    return jsonify(unit_amenities_schema.dump(amenities))
-
-
-@app.route("/unit_amenities/<int:amenity_id>", methods=["GET"])
-def get_unit_amenity(amenity_id):
-    amenity = UnitAmenity.query.get_or_404(amenity_id)
-    return jsonify(unit_amenity_schema.dump(amenity))
-
-
-@app.route("/unit_amenities", methods=["POST"])
-def create_unit_amenity():
-    data = request.get_json()
-    amenity = UnitAmenity(
-        name=data["name"],
-        description=data.get("description"),
-        iconUrl=data.get("iconUrl"),
-    )
-    db.session.add(amenity)
-    db.session.commit()
-    return jsonify(unit_amenity_schema.dump(amenity)), 201
-
-
-@app.route("/unit_amenities/<int:amenity_id>", methods=["DELETE"])
-def delete_unit_amenity(amenity_id):
-    amenity = UnitAmenity.query.get_or_404(amenity_id)
-    db.session.delete(amenity)
-    db.session.commit()
-    return "", 204
-
-
-# ---------- unit_amenities_joining ----------
-
-@app.route("/unit_amenities_joining", methods=["GET"])
-def get_unit_amenities_joining():
-    links = UnitAmenityJoining.query.all()
-    return jsonify(unit_amenities_joining_schema.dump(links))
-
-
-@app.route("/units/<int:unit_id>/amenities", methods=["GET"])
-def get_amenities_for_unit(unit_id):
-    links = UnitAmenityJoining.query.filter_by(unitId=unit_id).all()
-    return jsonify(unit_amenities_joining_schema.dump(links))
-
-
-@app.route("/unit_amenities_joining", methods=["POST"])
-def link_unit_amenity():
-    data = request.get_json()
-    link = UnitAmenityJoining(
-        unitId=data["unitId"],
-        amenityId=data["amenityId"],
-    )
-    db.session.add(link)
-    db.session.commit()
-    return jsonify(unit_amenity_joining_schema.dump(link)), 201
-
-
-@app.route("/unit_amenities_joining/<int:link_id>", methods=["DELETE"])
-def unlink_unit_amenity(link_id):
-    link = UnitAmenityJoining.query.get_or_404(link_id)
-    db.session.delete(link)
-    db.session.commit()
-    return "", 204
-
-
-# ---------- units ----------
-
-@app.route("/units", methods=["GET"])
-def get_units():
-    units = Unit.query.all()
-    result = []
-    for unit in units:
-        unit_data = unit.to_dict()
-        unit_data["amenities"] = [
-            link.amenity.to_dict() for link in unit.amenity_links
-        ]
-        result.append(unit_data)
-    return jsonify(result)
 
 #-------------get/properties for a particular manager------------------
 @app.route('/manager-properties/<int: id>')
