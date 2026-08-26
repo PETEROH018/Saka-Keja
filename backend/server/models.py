a7def2a94b6253c6dbf9b7a4231799693c110701
PETEROH018 <mathengep63@gmail.com>
2026-08-26 10:40:08 +0300

    Imported the configurations from the configurations folder

--- backend/server/models.py
+++ backend/server/models.py
@@ -1,24 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import (
-    Column,
-    Integer,
-    String,
-    Boolean,
-    ForeignKey,
-    MetaData,
-    VARCHAR,
-    DateTime,
-)
-from sqlalchemy.orm import relationship, declarative_base
-from sqlalchemy.ext.hybrid import hybrid_property
-from flask_bcrypt import Bcrypt
-from datetime import datetime, timezone
-
-
-meta = MetaData()
-bcrypt = Bcrypt()
-Base = declarative_base(metadata=meta)
-db = SQLAlchemy(metadata=meta)
+from configs import *
 
 class ApartmentOwner(Base):
     __tablename__ = "apartment_owners"
