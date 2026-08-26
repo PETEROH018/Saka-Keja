69695b719505fb3d8aa472ec94feb0660240e59e
PETEROH018 <mathengep63@gmail.com>
2026-08-26 15:00:06 +0300

    Added imports and configurations for the connection between the Flask app and the PostgreSQL database

--- backend/server/configs.py
+++ backend/server/configs.py
@@ -1,3 +1,6 @@
+import os
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import (
     Column,
@@ -19,3 +22,11 @@ meta = MetaData()
 bcrypt = Bcrypt()
 Base = declarative_base(metadata=meta)
 db = SQLAlchemy(metadata=meta)
+
+LOCAL_DB_URI = "postgresql+psycopg2://postgres:1234@localhost:5432/saka_keja"
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", LOCAL_DB_URI)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+CORS(app)  # allows your Vite frontend on localhost:5173 to call this API
