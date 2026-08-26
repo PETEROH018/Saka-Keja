9707bfc15b8226c1cae36f699f280cbb3363513c
Natalie Kimani <anitakimani07@gmail.com>
2026-08-26 22:12:11 +0300

    feat: import ApartmentAmenity model

--- backend/server/schema.py
+++ backend/server/schema.py
@@ -5,3 +5,4 @@ from models.Apartment import Apartment
 from models.Unit import Unit
 from models.UnitAmenity import UnitAmenity
 from models.UnitAmenityJoining import UnitAmenityJoining
+from models.ApartmentAmenity import ApartmentAmenity
