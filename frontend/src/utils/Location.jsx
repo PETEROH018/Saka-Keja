ec6724d3220bc0638e1359608f45fcae4f372600
Darrel <darrel124568@gmail.com>
2026-08-27 16:46:25 +0300

    Refactor Location component to accept location name as a parameter and validate input

--- frontend/src/utils/Location.jsx
+++ frontend/src/utils/Location.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const Location = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Location = async (locationName) => {
+  if (!locationName || typeof locationName !== 'string') {
+    throw new Error('Please provide a valid location name.');
+  }
+
+  const query = encodeURIComponent(locationName.trim());
+  const url = `https://nominatim.openstreetmap.org/search?format=json&q=${query}&limit=1`;
+
+  
 }
 
 export default Location
