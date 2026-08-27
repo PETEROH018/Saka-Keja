a9b809020ba252627233fd67d578760c5a7b9d44
Enoch-Wanjala <enochwanjala.97@gmail.com>
2026-08-27 18:26:00 +0300

    Separate authentication context files

--- frontend/src/context/AuthProvider.jsx.jsx
+++ frontend/src/context/AuthProvider.jsx.jsx
@@ -1,17 +1,28 @@
-import { createContext, useState } from "react";
-
-export const AuthContext = createContext(null);
+import { useState } from "react";
+import { AuthContext } from "./AuthContext";
 
 
 export function AuthProvider({ children }) {
 
     const [user, setUser] = useState(null);
 
+
+    const login = (userData) => {
+        setUser(userData);
+    };
+
+
+    const logout = () => {
+        setUser(null);
+    };
+
+
     return (
         <AuthContext.Provider
             value={{
                 user,
-                setUser
+                login,
+                logout
             }}
         >
             {children}
