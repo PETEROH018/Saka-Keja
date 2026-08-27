1485fecee9a5e9721eb2724c2ca898df89e23c80
jst.talel <mathewstalel@outlook.com>
2026-08-28 00:13:32 +0300

    Inputted imports for UnitDetails

---
+++ frontend/src/pages/UnitDetails/UnitDetails.jsx
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
+import './UnitDetails.css';
