0d26958429160ef8796a204d8bf149426e796771
PETEROH018 <mathengep63@gmail.com>
2026-08-28 12:45:48 +0300

    Added a check to ensure that the JSON payload from the client is not empty

--- backend/server/app.py
+++ backend/server/app.py
@@ -230,7 +230,10 @@ def update_unit(id):
 
     if not unit:
         return jsonify({"error": "Unit not found"}), 404
+    data=request.get_json()
     
+    if not data:
+            return jsonify({"error": "No data provided"}), 400
 
 
 
