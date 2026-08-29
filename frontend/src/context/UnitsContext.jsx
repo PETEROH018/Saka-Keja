788cc70afbf404bf92463bf6c1a450de316761e6
PETEROH018 <mathengep63@gmail.com>
2026-08-29 18:11:34 +0300

    declared and exported the UnitsProvider function and fetched the units from the backend using the /units endpoint

--- frontend/src/context/UnitsContext.jsx
+++ frontend/src/context/UnitsContext.jsx
@@ -2,3 +2,18 @@ import { createContext, useEffect } from "react";
 
 const UnitsContext = createContext()
 
+export default function UnitsProvider({children}){
+
+    const [allUnits,setAllUnits] = useState()
+
+    useEffect(()=>{
+        fetch('http://127.0.0.1:5000/units')
+        .then(response => response.json())
+        .then(data => setAllUnits(data))
+    },[])
+
+    return(
+        <></>
+    )
+
+} 
