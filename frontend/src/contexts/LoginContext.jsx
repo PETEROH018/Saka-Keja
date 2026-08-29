1d82197343e05236fc82adb571966d3f525d08cc
Darrel <darrel124568@gmail.com>
2026-08-29 08:49:20 +0300

    Add user state management to LoginContextProvider

--- frontend/src/contexts/LoginContext.jsx
+++ frontend/src/contexts/LoginContext.jsx
@@ -1,7 +1,13 @@
-import { createContext } from "react"
+import { createContext, useState } from "react"
 
 const LoginContext = createContext()
 
 export function LoginContextProvider({children}) {
-    
+
+    const [user, setUser] = useState(null)
+    return(
+        <LoginContext.Provider value={{user, setUser}}>
+            {children}
+        </LoginContext.Provider>
+    )
 }
