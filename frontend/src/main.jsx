6ce70b20e3f0787ef3f4367f0a24628744686a51
PETEROH018 <mathengep63@gmail.com>
2026-08-29 18:26:06 +0300

    Added curly braces around the UnitsProvider since it is not a default export in the UnitsContext.jsx file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthProvider.jsx'
-import UnitsProvider from './context/UnitsContext.jsx'
+import { UnitsProvider } from './context/UnitsContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
