3fd81ff52ab8754d9597b048613c45719ba0cc5f
PETEROH018 <mathengep63@gmail.com>
2026-08-30 21:09:00 +0300

    Created and configured an image uploader utility component for uploading images to Cloudinary and generating URLs for them that will be stored in the backend

---
+++ frontend/src/utils/ImageUploader.jsx
@@ -0,0 +1,8 @@
+export default function ImageUploader() {
+    const [image, setImage] = useState(null);
+    const [imageUrl, setImageUrl] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const CLOUD_NAME = "pdxdbkuz"; 
+    const UPLOAD_PRESET = "fofv56oc"; 
+}
