fd05f523febdc31a80c1369d3710e402e23986ba
PETEROH018 <mathengep63@gmail.com>
2026-09-03 22:36:31 +0300

    Added a protected routes component that wraps around private routes that should be accessed by only the owner or the student

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import ApartmentDetails from './pages/ApartmentDetails'
 import SearchResults from './pages/SearchResults/SearchResults'
 import AddApartment from './pages/AddApartment/AddApartment'
 import MyProperties from './pages/MyProperties/MyProperties.jsx'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes, Navigate, Outlet } from 'react-router-dom'
 import ComparePage from "./pages/Compare/ComparePage"
 import StudentProfile from "./components/StudentProfile/StudentProfile.jsx"
 import StudentEditProfile from "./components/EditProfile/StudentEditProfile.jsx"
@@ -21,42 +21,65 @@ import UnitDetails from './pages/UnitDetails/UnitDetails';
 import EditUnit from './pages/EditUnit/EditUnit.jsx'  
 import StudentDashboard from './pages/StudentDashboard/StudentDashboard'
 import UnitListingForm from './pages/UnitListingForm/UnitListingForm.jsx'
+import { useAuth } from './context/useAuth.jsx'
+
+const ProtectedRoute = ({ allowedRoles, user }) => {
+  // Check if user is logged in
+  if (!user) {
+    return <Navigate to="/auth" replace />;
+  }
+
+  // Check if user has the correct role
+  if (allowedRoles && !allowedRoles.includes(user.role)) {
+    return <Navigate to="/" replace />;
+  }
+
+  // If logged in and role matches, render the child routes
+  return <Outlet />;
+};
 
 function App() {
+  const { user } = useAuth();
   return (
     <HashRouter>
+      
+     
       <Routes>
+        {/* Pulic routes */}
         <Route path="/" element={<Home />} />
-        <Route path="/admin-dash" element={<AdminDashboard />} />
         <Route path="/apartment-details/:id" element={<ApartmentDetails />} />
         <Route path="/search" element={<SearchResults />} />
-        <Route path="/my-properties" element={<MyProperties />} />
-        <Route path="/edit-property/:id" element={<EditProperty />} />
         <Route path="/compare" element={<ComparePage />} />
-        <Route path="/edit-unit/:id" element={<EditUnit />} />
-
-        {/* Student Profile & Edit Routes */}
-        <Route path="/student-profile" element={<StudentProfile />} />
-        <Route path="/StudentProfile" element={<StudentProfile />} />
-        <Route path="/StudentEditProfile" element={<StudentEditProfile />} />
-        <Route path="/student-edit-profile" element={<StudentEditProfile />} />
-        <Route path="/edit-profile" element={<StudentEditProfile />} />
-
-        <Route path="/owner-profile" element={<OwnerProfile />} />
         <Route path="/owner/units/add" element={<UnitListingForm/>}/>
-        <Route path="/saved-properties" element={<SavedProperties />} />
         <Route path="/auth" element={<AuthPage />} />
-
-        {/* Manager Profile & Edit Routes */}
-        <Route path="/manager-profile" element={<ManagerProfile />} />
-        <Route path="/ManagerEditProfile" element={<ManagerEditProfile />} />
-
-        <Route path='/add-apartment' element={<AddApartment />} />
         <Route path="/about" element={<About />} />
         <Route path="/available-units/:id" element={<AvailableUnits />} />
-        <Route path="/student-dashboard" element={<StudentDashboard />} />
+        <Route path="/unit-details/:id" element={<UnitDetails />} /> 
+
+         {/* Owner only routes */}
+        <Route element={<ProtectedRoute user={user} allowedRoles={['owner']} />}>
+          <Route path="/admin-dash" element={<AdminDashboard />} />
+          <Route path="/my-properties" element={<MyProperties />} />
+          <Route path="/edit-property/:id" element={<EditProperty />} />
+          <Route path="/edit-unit/:id" element={<EditUnit />} />
+          <Route path="/manager-profile" element={<ManagerProfile />} />
+          <Route path="/ManagerEditProfile" element={<ManagerEditProfile />} />
+          <Route path='/add-apartment' element={<AddApartment />} />
+        </Route>
+        {/* Student Only Routes */}
+        <Route element={<ProtectedRoute user={user} allowedRoles={['student']} />}>
+          <Route path="/student-profile" element={<StudentProfile />} />
+          <Route path="/StudentProfile" element={<StudentProfile />} />
+          <Route path="/StudentEditProfile" element={<StudentEditProfile />} />
+          <Route path="/student-edit-profile" element={<StudentEditProfile />} />
+          <Route path="/edit-profile" element={<StudentEditProfile />} />
+          <Route path="/student-dashboard" element={<StudentDashboard />} />
+        </Route>        
+
+        {/* <Route path="/owner-profile" element={<OwnerProfile />} /> */}
+        {/* <Route path="/saved-properties" element={<SavedProperties />} /> */}
         {/* <Route path="/apartments/:apartmentId/units/:unitId" element={<UnitDetails />} /> */}
-        <Route path="/unit-details/:id" element={<UnitDetails />} />      </Routes>
+      </Routes>
     </HashRouter>
 
   )
